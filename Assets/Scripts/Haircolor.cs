b0c605c5ae274d5d972e0130c69ca02b006a9f66
Ju <j.jouandon@mtp.ecolescreatives.com>
2025-01-09 10:19:40 +0100

    Ju 09/01 water collider
    ajout de tous les cube autour des zones d'eau + modif de la map

--- Assets/Scripts/Haircolor.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Haircolor : MonoBehaviour //script comme pour les genes pour créer un truc d'hérédité etc
-{
-   
-}
